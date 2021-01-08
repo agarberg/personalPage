2d7a1f9c933bc1ca81966be5e416786db41c765f
Myron <dubtwee@gmail.com>
2021-01-08 10:11:29 -0600

    convert App component to functional component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-import Animals from '../Animals/Animals.js'
-import Plants from '../Plants/Plants.js'
-import Home from '../Home/Home.js'
+import Animals from '../Animals/Animals.js';
+import Plants from '../Plants/Plants.js';
+import Home from '../Home/Home.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>SPAs!</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>SPAs!</h1>
+    </div>
+  );
 }
 
 export default App;
