f4aba0a3bf65dd7d305ff303eb525b50237618f4
DoctorHowser <doctorhowser@gmail.com>
2021-01-14 10:31:49 -0600

    use .jsx

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import Animals from '../Animals/Animals.js';
-import Plants from '../Plants/Plants.js';
-import Home from '../Home/Home.js';
+import Animals from '../Animals/Animals.jsx';
+import Plants from '../Plants/Plants.jsx';
+import Home from '../Home/Home.jsx';
 
 function App() {
   return (
